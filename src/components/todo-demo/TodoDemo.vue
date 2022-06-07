740ef84d6a3f5f6c6ef152b65e2564ec376dbe1a
camphorswood <2191964232@qq.com>
2022-06-07 14:43:27 +0800

    实现静态TODO

---
+++ src/components/todo-demo/TodoDemo.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="todo-container">
+    <div class="todo-warp">
+      <TodoHeader/>
+      <TodoList/>
+      <TodoFooter/>
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoHeader from "@/components/todo-demo/TodoHeader";
+import TodoList from "@/components/todo-demo/TodoList";
+import TodoFooter from "@/components/todo-demo/TodoFooter";
+
+export default {
+  name: "TodoDemo",
+  components: {TodoHeader, TodoList, TodoFooter}
+}
+</script>
+
+<style scoped>
+@import url(./main.css);
+.todo-container {
+  width: 600px;
+  margin: 0 auto;
+}
+
+.todo-container .todo-warp {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+}
+</style>
---
+++ src/components/todo-demo/TodoList.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul class="todo-main">
+    <TodoItem/>
+  </ul>
+</template>
+
+<script>
+import TodoItem from "@/components/todo-demo/TodoItem";
+
+export default {
+  name: "TodoList",
+  components: {TodoItem}
+}
+</script>
+
+<style  scoped>
+.todo-main {
+  margin-left: 0;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+</style>
